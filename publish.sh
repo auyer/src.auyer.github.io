dbfd608cf10d19de13695b1d6d72784bb5564ad0
auyer <rafael@rcpassos.me>
2019-10-23 01:41:59 -0300

    update published version and publish script

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo -t base16 # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hello-friend # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To publish folder
 cd publish
